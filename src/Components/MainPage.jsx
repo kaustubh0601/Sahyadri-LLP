68155831a38d0d1f4e8259b007131194e9c58bea
Kaustubh Kumbharkar <144944896+kaustubh0601@users.noreply.github.com>
2024-06-28 15:05:17 +0530

    Uploading

---
+++ src/Components/MainPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../Styles/MainPage.css'
+import Statisticsbar from './Statisticsbar'
+import WhatWeDo from './WhatWeDo'
+import LandmarksMainPage from './LandmarksMainPage'
+import OurClients from './OurClients'
+export default function MainPage() {
+  return (
+    <>
+      <div className="main-container">
+        <div className="main-container-content">
+        <h2>From Ideation <br /> to Realization </h2>
+        <p>As Pune's fastest-growing contract-based construction company with 20 years of industry expertise, we excel in delivering cutting-edge construction services. Our commitment to value, driven by advanced technology and a discerning vision, sets us apart in the sector.</p>
+        {/* <button className='read-more-button'>Read More</button> */}
+        </div>
+      </div>
+      <Statisticsbar/>
+      <WhatWeDo/>
+      <LandmarksMainPage/>
+      <OurClients/>
+    </>
+  )
+}
